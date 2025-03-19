5305eed9409e7e4e48afa0aae59e3bbeee1a3641
mariancastillo <marianocastillo08@gmail.com>
2025-03-19 10:05:58 -0400

    Crear componente contador

---
+++ src/components/ContadorVue.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <center>
+<h1>ESTO ES UN CONTADOR</h1>
+<h1><P>{{ contador }}</P></h1>
+
+<button @click="incrementar">Incrementar</button>
+&nbsp; &nbsp;
+<button @click="decrementar">Decrementar</button>
+</center>
+</template>
+
+
+
+
+<script setup>
+import { ref } from 'vue';
+
+
+const contador = ref(0);
+const incrementar = () => {
+  contador.value+=5;
+}
+
+const decrementar = () => {
+  contador.value-=5;
+}
+
+</script>
+
+
+
+
+
+<style scoped>
+p{
+  color: blue;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,8 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+
+    
     {
       path: '/about',
       name: 'about',
